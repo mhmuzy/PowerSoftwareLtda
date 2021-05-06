399c72ab9720d8af67bb91b9fa05473b79d0659f
marcio henrique <mhmuzy@embratel.com.br>
2021-05-06 13:54:53 -0300

    Finalização do Programa [Treinamento]

--- src/Hitss/Treinamento/Infra/Controller.java
+++ src/Hitss/Treinamento/Infra/Controller.java
@@ -15,6 +15,7 @@ public class Controller {
 		System.out.println("");
 		for (int i = 1; i < Salario.length; i++) {
 			CalculaImposto(i, Salario[i]);
+			CalculaInss(i, Salario[i]);
 		}
 		System.out.println("");
 		System.out.println("-------------------------------------------------------------------------------------------");
@@ -23,10 +24,44 @@ public class Controller {
 	}
 	
 	public static void CalculaImposto(int mat, double sal) {
+		double desconto = 0.0;
 		
+		if(sal <= 1903.98) {
+		desconto = sal;
+			System.out.println("O Salário do Funcionário da Matrícula " + mat + " Com o Desconto de Imposto de Renda Foi de R$ " + desconto);
+		} else if(sal >= 1903.99 && sal <= 2826.65) {
+			
+			desconto = sal * 0.925;
+			System.out.println("O Salário do Funcionário da Matrícula " + mat + " Com o Desconto de Imposto de Renda Foi de R$ " + desconto);
+			
+		} else if(sal >= 2826.66 && sal <= 3751.05) {
+			
+			desconto = sal * 0.85;
+			System.out.println("O Salário do Funcionário da Matrícula " + mat + " Com o Desconto de Imposto de Renda Foi de R$ " + desconto);
+		} else if(sal >= 3751.06 && sal <= 4664.68) {
+			
+			desconto = sal * 0.775;
+			System.out.println("O Salário do Funcionário da Matrícula " + mat + " Com o Desconto de Imposto de Renda Foi de R$ " + desconto);
+		} else {
+			desconto = sal * 0.725;
+			System.out.println("O Salário do Funcionário da Matrícula " + mat + " Com o Desconto de Imposto de Renda Foi de R$ " + desconto);
+		}
+		}
+	
+	public static void CalculaInss(int mat, double sal) {
+		double desconto = 0.0;
 		
-		double desconto = sal * 0.925;
-		System.out.println("O Salário do Funcionário da Matrícula " + mat + " Com o Desconto de Imposto de Renda Foi de R$ " + desconto);
-		
-	}
+		if(sal <= 1693.72) {
+		desconto = sal * 0.92;
+			System.out.println("O Salário do Funcionário da Matrícula " + mat + " Com o Desconto do INSS Foi de R$ " + desconto);
+		} else if(sal >= 1693.73 && sal <= 2822.90) {
+			
+			desconto = sal * 0.91;
+			System.out.println("O Salário do Funcionário da Matrícula " + mat + " Com o Desconto do INSS Foi de R$ " + desconto);
+			
+		} else {
+			desconto = sal * 0.89;
+			System.out.println("O Salário do Funcionário da Matrícula " + mat + " Com o Desconto do INSS Foi de R$ " + desconto);
+		}
+		}
 }
